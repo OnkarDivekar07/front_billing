8245d8c4e06ff2d6c9c7ca72298422ce15d97b64
sushmabhapkar <sushmabhapkar244@gmail.com>
2026-04-01 11:11:07 +0530

    callback removed

--- src/components/BillingModal.jsx
+++ src/components/BillingModal.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect} from "react";
 import { submitBilling } from "../api/endpoints";
 import { extractError } from "../utils/extractError";
 import { useToast } from "./Toast";
